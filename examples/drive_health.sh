2ca8655071dd5d1821162493523996bd1561810b
Spoofing <spoofing@spfng.com>
2024-09-27 23:24:59 +0300

    Update drive_health.sh

--- examples/drive_health.sh
+++ examples/drive_health.sh
@@ -3,28 +3,25 @@
 drive_health () {
 	local disk="$1"
 	local temp=$(mktemp XXXXXXXXXX)
-	local next="/var/www/html/ready/$temp"
+	local next="/var/www/html/drive_health/$temp"
 
-	smartctl --all --vendorattribute="1,raw48:54" --vendorattribute="7,raw48:54" --vendorattribute="195,raw48:54" "/dev/$disk" 1>"$temp" 2>"$temp"
+	smartctl --all --vendorattribute="1,raw48:54" --vendorattribute="7,raw48:54" --vendorattribute="195,raw48:54" "$disk" 1>"$temp" 2>"$temp"
 
 	error="$?"
 	if test "$error" = "0"; then
 		local temp_size=$(stat --format "%s" "$temp")
 		local serial_number=$(cat "$temp" | awk '{if ($1 == "Serial" && ($2 == "Number:" || $2 == "number:")) { print $NF; } }')
 
-		echo "X-Page-Title: Disk $serial_number assigned to $disk" | cat - "$temp" | tee "$temp"
-		echo "X-Page-Size: $temp_size" | cat - "$temp" | tee "$temp"
-		echo "" | cat - "$temp" | tee "$temp"
-
 		install -D -m 0644 -o "www-data" -g "www-data" "$temp" "$next"
-
-		echo "$next"
 	fi
 
 	rm "$temp"
 }
 
 main () {
+
+	# init
+
 	if test -x "$(command -v smartctl)"; then
 		:
 	else
@@ -32,13 +29,89 @@ main () {
 		exit 1
 	fi
 
-	for disk in $(lsblk --include 8,259 --nodeps --bytes --output KNAME --noheadings); do
+	# Check disks attached to the board directly or in passthrough
 
-		uploaded_file=$(drive_health "$disk")
+	echo "Checking NVMe drives..."
 
+	for disk in $(find "/dev" -type "b" -name "nvme?*n?*"); do
+		(
+		drive_health "$disk"
+		) & disown
 	done
 
-	return "$error"
+	echo "Checking directly attached drives..."
+
+	for disk in $(find "/dev" -type "b" -name "sd?*"); do
+		(
+		drive_health "$disk"
+		) & disown
+	done
+
+	# Check disks attached to the board directly or in passthrough (BSD)
+
+	echo "Checking directly attached drives (passthrough, BSD)..."
+
+	for disk in $(find "/dev" -type "c" -name "pass?*"); do
+		(
+		drive_health "$disk"
+		) & disown
+	done
+
+	# Check disks behind LSISAS2008 LV
+
+	echo "Checking LSISAS2008 drives..."
+
+	for disk in $(find "/dev" -type "c" -name "sg?*"); do
+		(
+		drive_health "$disk"
+		) & disown
+	done
+
+	# Check disks behind a 3ware card
+
+	echo "Checking 3ware drives..."
+
+	if test -f "/dev/twl0"; then
+		for index in {0..20}; do
+			(
+			drive_health "-d 3ware,$index /dev/twl0 -T permissive"
+			) & disown
+		done
+	fi
+
+	# Check scsi disks behind an lsi card - fixed at sda at the moment
+
+	echo "Checking LSI drives..."
+
+	if test $(command -v lspci); then
+		if test "$(lspci | grep -i LSI | wc -l)" == "0"; then
+			:
+		else
+			for index in {0..20}; do
+				(
+				drive_health "-d megaraid,$index /dev/sda -T permissive"
+				) & disown
+			done
+
+			for index in {0..20}; do
+				(
+				drive_health "-d sat+megaraid,$index /dev/sda -T permissive"
+				) & disown
+			done
+		fi
+	fi
+
+	# Check scsi disks behind an HPcard - fixed at sda at the moment
+
+	echo "Checking HP/HPE drives..."
+
+	for index in {0..20}; do
+		(
+		drive_health "-d cciss,$index /dev/sda -T permissive"
+		) & disown
+	done
+
+	return 0
 }
 
 main "$@"
